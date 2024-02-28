7fcced5d46bf8f5e7e223884fb2c93af8e9b85f0
nasimakhtar <nasim@backbase.com>
2024-02-28 11:40:47 +0530

    Open Api Specification added

--- purchase/src/main/java/com/skiply/purchase/PurchaseApplication.java
+++ purchase/src/main/java/com/skiply/purchase/PurchaseApplication.java
@@ -1,11 +1,14 @@
 package com.skiply.purchase;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition(info = @Info(title = "Purchase Api", version = "v1"))
 public class PurchaseApplication {
 
 	public static void main(String[] args) {
